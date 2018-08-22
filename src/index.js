9cbc2fa9d9dffcfcceacc1d06d6000f85c15018c
ema18787 <emanueljtc@gmail.com>
2018-08-22 12:55:44 -0400

    importando y conf firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDt2GFLVyIX9WV5ZfMRlaFRnVM8as_Prvo",
+  authDomain: "moments-f8a39.firebaseapp.com",
+  databaseURL: "https://moments-f8a39.firebaseio.com",
+  projectId: "moments-f8a39",
+  storageBucket: "moments-f8a39.appspot.com",
+  messagingSenderId: "89180780233"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
